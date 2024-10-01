9e63bac507a5d05fb9ca23b855b8ae219c432f88
Viktor Vorona <viktor.vorona@sonarsource.com>
2024-10-01 13:37:22 +0200

    TEST

--- src/backend.js
+++ src/backend.js
@@ -8,5 +8,5 @@ crypto.createCipheriv("DES", key, iv); // Noncompliant
 
 location.href = "javascript:void(0)"; // Sensitive
 
-// AZBURwZL83ztCUnc4EG6
+// AZBURwZL83ztCUnc4EG612
 // AZBURwZL83ztCUnc4EG5
