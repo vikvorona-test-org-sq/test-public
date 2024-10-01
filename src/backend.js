09d0665684dd90c593b9a3b5a8834222e7a19e26
Viktor Vorona <viktor.vorona@sonarsource.com>
2024-10-01 11:09:12 +0200

    Vuln

--- src/backend.js
+++ src/backend.js
@@ -4,6 +4,9 @@ const iv = "12345";
 
 // test
 // lol
+crypto.createCipheriv("DES", key, iv); // Noncompliant
+
+location.href = "javascript:void(0)"; // Sensitive
 
 // AZBURwZL83ztCUnc4EG6
 // AZBURwZL83ztCUnc4EG5
