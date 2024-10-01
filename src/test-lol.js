8397565faf395ae8667f234c11b86b92446a573c
Viktor Vorona <viktor.vorona@sonarsource.com>
2024-10-01 14:15:00 +0200

    TEST ALERTS

---
+++ src/test-lol.js
@@ -0,0 +1,12 @@
+const crypto = require("crypto");
+const key = "123";
+const iv = "12345";
+
+// test
+// lol
+crypto.createCipheriv("DES", key, iv); // Noncompliant
+
+location.href = "javascript:void(0)"; // Sensitive
+
+// AZBURwZL83ztCUnc4EG6
+// AZBURwZL83ztCUnc4EG5
